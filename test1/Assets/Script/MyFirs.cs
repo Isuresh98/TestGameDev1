4e33f7818103688cffe41a47301862d6146f2811
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2022-04-26 23:13:07 +0530

    assert

---
+++ test1/Assets/Script/MyFirs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirs : MonoBehaviour
+{
+   public int myAge =15;
+   public int legalAgeToDrive=18;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(myAge>=legalAgeToDrive)
+        {
+            print("You Can Dive");
+
+        }
+        else
+        {
+            print("You Can't Drive");
+
+        }
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
