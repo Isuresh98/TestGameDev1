59eb8769df7c434449fd1f2d14d0f1c42bd6e59b
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2022-04-27 16:52:01 +0530

    Spacebar Test

---
+++ FistMoveSpaceBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistMoveSpaceBar : MonoBehaviour
+{
+    public int Number = 10;
+    public float KeyInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyInput =Input.GetAxis("Jump");
+
+
+        if(KeyInput>0)
+        {
+            Number++;
+            print(Number);
+        }
+    }
+}
